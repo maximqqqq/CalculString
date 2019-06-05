7f26a2ebd86fea7a0967e7a27229cdc365ecc9df
Max <https://github.com/maximqqq>
2019-06-05 23:00:12 +0300

    I was looking for a mistake where it is not)))

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) {
 
-        System.out.println(Calculator.ExpressionToRPN("2+2*2"));
+        System.out.println(Calculator.ExpressionToRPN("(222+2)*2"));
+        System.out.println(Calculator.RPNtoAnswer(Calculator.ExpressionToRPN("(2+2)*2")));
 
     }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -8,34 +8,63 @@ public class Calculator {
 
         int priorety;//Текущий преоритет символа
 
-        for (int i = 0; i< Expr.length();i++){
-         priorety = getP(Expr.charAt(i));//Проверяем преоритеты, создали эту перем чтобы пост не вызывать getP
+        for (int i = 0; i < Expr.length(); i++) {
+            priorety = getP(Expr.charAt(i));//Проверяем преоритеты, создали эту перем чтобы пост не вызывать getP
 
             if (priorety == 0) curent += Expr.charAt(i);
             if (priorety == 1) stack.push(Expr.charAt(i));
 
-            if (priorety >1){//роверим непустой ли стек, запускаем while
-                curent +=" ";
-                while (!stack.empty()){//!empty непустой
-                    if (getP(stack.peek()) >= priorety)curent += stack.pop(); //peek берем верхний символ pop удоляем
+            if (priorety > 1) {//роверим непустой ли стек, запускаем while
+                curent += " ";
+                while (!stack.empty()) {//!empty непустой
+                    if (getP(stack.peek()) >= priorety)curent += stack.pop(); //peek берем верхний символ pop удоляем, берем из стека
                     else break;
                 }
                 stack.push(Expr.charAt(i));
             }
 
-            if (priorety == -1){
-                curent = " ";
+            if (priorety == -1) {
+                curent += " ";
                 while (getP(stack.peek()) != 1) curent += stack.pop();
 
                 stack.pop();
             }
         }
-        while (!stack.empty()) curent+= stack.pop();
+        while (!stack.empty()) curent += stack.pop();
         return curent;
     }
 
+
+
+
     public static double RPNtoAnswer(String rpn) {
-        return 0;
+        String operand = new String();
+        Stack<Double> stack = new Stack<>();
+
+        for (int i = 0; i < rpn.length(); i++) {
+            if (rpn.charAt(i) == ' ') continue;
+
+            if (getP(rpn.charAt(i)) == 0) {
+                while (rpn.charAt(i) != ' ' && getP(rpn.charAt(i)) == 0) {
+                    operand += rpn.charAt(i++);
+                    if (i == rpn.length()) break;
+                }
+
+                stack.push(Double.parseDouble(operand));
+                operand = new String();
+            }
+
+            if (getP(rpn.charAt(i)) > 1) {
+                double a = stack.pop();
+                double b = stack.pop();
+
+                if (rpn.charAt(i) == '+') stack.push(b + a);
+                if (rpn.charAt(i) == '-') stack.push(b - a);
+                if (rpn.charAt(i) == '*') stack.push(b * a);
+                if (rpn.charAt(i) == '/') stack.push(b / a);
+            }
+        }
+        return stack.pop();
     }
 
     private static int getP(char token) {                   //Определяем преоритеты символов
